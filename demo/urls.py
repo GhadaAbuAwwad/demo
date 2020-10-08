ee7deb86793f120ef58b054e02ea21c9c88c4e81
Ghada <ghada.j@najah.edu>
2020-10-08 15:28:30 +0300

    "blogs and static changes"

--- demo/urls.py
+++ demo/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('blogs/', include('blogs.urls')),
     path('admin/', admin.site.urls),
 
 ]
